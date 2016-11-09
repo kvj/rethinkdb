c4cb7886187234e9dc42eabc2565f98dbd3fd430
Konstantin Vorobyev <vorobyev@coacha.com>
2016-11-09 11:07:36 +0900

    Do same for other ssl related ifdefs

--- src/crypto/initialization_guard.cc
+++ src/crypto/initialization_guard.cc
@@ -17,13 +17,8 @@
 namespace crypto {
 
 initialization_guard_t::initialization_guard_t() {
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
     SSL_library_init();
     SSL_load_error_strings();
-#else
-    OPENSSL_init_ssl(0, nullptr);
-    OPENSSL_init_crypto(0, nullptr);
-#endif
 
     // Make OpenSSL thread-safe by registering the required callbacks
     CRYPTO_THREADID_set_callback([](CRYPTO_THREADID *thread_out) {
@@ -49,11 +44,7 @@ initialization_guard_t::initialization_guard_t() {
 }
 
 initialization_guard_t::~initialization_guard_t() {
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
     ERR_free_strings();
-#else
-    OPENSSL_cleanup();
-#endif
 }
 
 }  // namespace crypto
